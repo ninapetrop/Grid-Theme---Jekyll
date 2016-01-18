3b14be60885ce5f157ecbd138e3d7cad5661e3ce
Nina Petropoulos <petropoulos.nina@gmail.com>
2016-01-17 21:52:24 -0500

    install bourbon, JS for hover animations

---
+++ _posts/2014-01-28-nike-flyknit-review.markdown
@@ -0,0 +1,11 @@
+---
+layout: default
+title:  "Nike Flyknit 3.0 Review"
+date:   2014-04-20 09:11:03
+categories: reviews
+description: Phasellus hendrerit. Pellent aliquet nibh nec urna. In nis aliquet vel, dapibus id,mattis.
+thumbnail: /assets/img/thumbnails/img-9.jpg
+caption: If you have a caption for an image, you should write it here
+---
+
+A Jekyll theme for blogging. With the ability to sort posts by category and list various authors in an authors block, this theme is perfect for collaborative projects.
--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,2 +1,20 @@
-var sidebar = document.getElementById('navbarSticky');
-Stickyfill.add(sidebar);
+$(function(){
+  articleView();
+
+});
+
+  function articleView(){
+    $('.preview-box').mouseover(function(){
+      $(this).removeClass('text-hidden').addClass('text-show');
+    });
+    $('.preview-box').mouseout(function(){
+      $(this).addClass('text-hidden').removeClass('text-show');
+    });
+  }
+
+  // function articleView(){
+  //   var randNum = Math.floor(Math.random() * $('.preview-box').length + 1)
+  //
+  //   $('.preview-box').eq(randNum).removeClass('text-hidden')
+  //     .siblings().addClass('text-hidden');
+    //}
